c19e93f8d4728a70100dc4358357ed5223ce518c
yacine camara <camaraya254@gmail.com>
2020-11-09 15:44:32 +0000

    camara

---
+++ simplon/yassk.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+   #include <stdlib.h>
+   {
+        int a,b,n_diviseurs;
+        for(a=0;a<=100;a++)
+
+   {   n_diviseurs=0;
+   for(b=1;j<=a;b++)
+   {
+        if(a%b==0)
+n_diviseurs=n_diviseurs+1;
+}
+if(n_diviseurs<=2)
+printf("%d est premier\n",a);
+ }
+ return 0;
+ }
+
+
+ 
